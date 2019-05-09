a94892a979f6a349540ca1bf48b39ad91f1fa272
iykekings <iykekings36@gmail.com>
2019-05-09 16:05:51 +0100

    Finish Prototype.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(personAttrs) {
+       this.name = personAttrs.name;
+       this.age = personAttrs.age;
+       this.location = personAttrs.location;
+       this.gender = personAttrs.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person {
+    constructor(insAttrs) {
+        super(insAttrs);
+        this.specialty = insAttrs.specialty;
+        this.favLanguage = insAttrs.favLanguage;
+        this.catchPhrase = insAttrs.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+    grade(student,subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
