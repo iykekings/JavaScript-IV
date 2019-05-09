b2cae7a57246db4fcf383aa714fca86b793fce79
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-10 00:17:52 +0100

    complete lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,6 +24,20 @@ class Instructor extends Person {
   grade(student, subject) {
     console.log(`${student.name} receives a perfect score on ${subject}`);
   }
+  changeGrade(student) {
+    const grade = randomNum(1, 100);
+    if (randomNum(1, 2) == 1) {
+      // ADD, makes sure the score doesn't go over 100
+      return student.grade + grade >= 100
+        ? (student.grade = 100)
+        : (student.grade += grade);
+    } else {
+      // SUBTRACT,  makes sure the score doesn't go below 0
+      return student.grade - grade <= 0
+        ? (student.grade = 0)
+        : (student.grade -= grade);
+    }
+  }
 }
 
 class Student extends Person {
@@ -31,6 +45,7 @@ class Student extends Person {
     this.previousBackground = stAttrs.previousBackground;
     this.className = stAttrs.className;
     this.favSubjects = stAttrs.favSubjects;
+    this.grade = sAttrs.grade;
   }
   listsSubjects() {
     this.favSubjects.forEach(singleSubject => console.log(singleSubject));
@@ -40,6 +55,17 @@ class Student extends Person {
   }
   sprintChallenge() {
     console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+  graduate() {
+    if (this.grade >= 70) {
+      console.log(`${this.name} has graduated from Lambdaschool`);
+    } else {
+      console.log(
+        `${
+          this.name
+        } is not yet ready to graduate from Lambdaschool, keep grading his assignments`
+      );
+    }
   }
 }
 
@@ -58,3 +84,33 @@ class ProjectManager extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+
+// helper functions
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Tests
+const newStudent = new Student({
+  name: 'Student1',
+  location: 'Freetown',
+  gender: 'Male',
+  age: 26,
+  previousBackground: 'Js',
+  className: 'webeu2',
+  favSubjetcs: ['CSS', 'JS', 'HTML'],
+  grade: 17
+});
+const newStudent = new ProjectManager({
+  name: 'PM1',
+  location: 'Freetown',
+  gender: 'Male',
+  age: 26,
+  specialty: 'Js',
+  favLanguage: 'Typescript',
+  catchPhrase: 'Valar Morghuilis',
+  gradClassName: 'lamda001',
+  favInstructor: 'Cabrejas'
+});
+
+// Console logs
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Prototype Refactor
 
@@ -25,15 +25,15 @@ Prototype Refactor
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-class GameObject{
-    constructor(attrs) {
+class GameObject {
+  constructor(attrs) {
     this.name = attrs.name;
     this.createdAt = attrs.createdAt;
     this.dimensions = attrs.dimensions;
-}
-destroy() {
+  }
+  destroy() {
     return `${this.name} was removed from the game.`;
-  };
+  }
 }
 
 /*
@@ -43,16 +43,15 @@ destroy() {
     * should inherit destroy() from GameObject's prototype
   */
 class CharacterStats extends GameObject {
-    constructor(charaAttrs) {
-        super(charaAttrs);
-        this.healthPoints = charaAttrs.healthPoints;
-    }
-    takeDamage() {
-        return `${this.name} took damage`;
-    }
+  constructor(charaAttrs) {
+    super(charaAttrs);
+    this.healthPoints = charaAttrs.healthPoints;
+  }
+  takeDamage() {
+    return `${this.name} took damage`;
+  }
 }
-  
-  
+
 /*
     === Humanoid (Having an appearance or character resembling that of a human.) ===
     * team
@@ -63,176 +62,179 @@ class CharacterStats extends GameObject {
     * should inherit takeDamage() from CharacterStats
   */
 class Humanoid extends CharacterStats {
-    constructor(humAttrs) {
-        super(humAttrs);
-        this.team = humAttrs.team;
-        this.weapons = humAttrs.weapons;
-        this.language = humAttrs.language;
-    }
-    greet() {
-        return `${this.name} offers a greeting in ${this.language}.`;
-    }
+  constructor(humAttrs) {
+    super(humAttrs);
+    this.team = humAttrs.team;
+    this.weapons = humAttrs.weapons;
+    this.language = humAttrs.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
 }
-  
-  /*
-   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-   * Instances of CharacterStats should have all of the same properties as GameObject.
-   */
-  
-  // Test you work by un-commenting these 3 objects and the list of console logs below:
-  
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: ['Staff of Shamalama'],
-    language: 'Common Tongue'
-  });
-  
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: ['Giant Sword', 'Shield'],
-    language: 'Common Tongue'
-  });
-  
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: ['Bow', 'Dagger'],
-    language: 'Elvish'
-  });
-  
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  
+
+/*
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: ['Staff of Shamalama'],
+  language: 'Common Tongue'
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: ['Giant Sword', 'Shield'],
+  language: 'Common Tongue'
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: ['Bow', 'Dagger'],
+  language: 'Elvish'
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
-class Hero extends Humanoid{
-    constructor(hAttrs) {
-        super(hAttrs);
-        this.strength = hAttrs.strength;
-    }
-    reduceHealthBy(severity) {
-        return this.healthPoints - severity > 0
-            ? (this.healthPoints -= severity)
-            : (() => {
-                this.healthPoints = 0;
-                return this.destroy();
-            })();
-    }
+class Hero extends Humanoid {
+  constructor(hAttrs) {
+    super(hAttrs);
+    this.strength = hAttrs.strength;
+  }
+  reduceHealthBy(severity) {
+    return this.healthPoints - severity > 0
+      ? (this.healthPoints -= severity)
+      : (() => {
+          this.healthPoints = 0;
+          return this.destroy();
+        })();
+  }
 }
-  
-  
+
 class Villain extends Humanoid {
-    constructor(vAttrs) {
-        super(vAttrs);
-        this.strength = vAttrs.strength;
-    }
-    // Villain's health get reduce by twice the severity
-    reduceHealthBy(severity) {
-        return this.healthPoints - severity * 2 > 0
-            ? (this.healthPoints -= severity * 2)
-            : (() => {
-                this.healthPoints = 0;
-                return this.destroy();
-            })();
-    }
+  constructor(vAttrs) {
+    super(vAttrs);
+    this.strength = vAttrs.strength;
+  }
+  // Villain's health get reduce by twice the severity
+  reduceHealthBy(severity) {
+    return this.healthPoints - severity * 2 > 0
+      ? (this.healthPoints -= severity * 2)
+      : (() => {
+          this.healthPoints = 0;
+          return this.destroy();
+        })();
+  }
 }
- 
-  // Creates a hero named thor, he gets a strength of 2
-  const thor = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4
-    },
-    healthPoints: 10000,
-    strength: 2,
-    name: 'Thor',
-    team: 'Forest Kingdom',
-    weapons: ['Bow', 'Dagger'],
-    language: 'Elvish'
-  });
-  
-  // Creates a villain named thanos, gets a strength of 4 for the infinity stones  
-  const thanos = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4
-    },
-    healthPoints: 10000,
-    strength: 4,
-    name: 'Thanos',
-    team: 'Forest Kingdom',
-    weapons: ['Bow', 'Dagger'],
-    language: 'Elvish'
-  });
-  
-  // Gaming helper functions
-  function randomIndex(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  // Starts a fight between two Humanoids
-  function combat(fighter1, fighter2) {
-    console.log(`Fight Started between ${fighter1.name} and ${fighter2.name}`);
-    let winner = '';
-    while (fighter1.healthPoints && fighter2.healthPoints) {
-      const index = randomIndex(1, 2);
-      if (index == 1) {
-        console.log(fighter2.takeDamage());
-        console.log(
-          `${fighter2.name} healthPoints remaining ${fighter2.reduceHealthBy(
-            fighter1.strength
-          )}`
-        );
-      } else {
-        console.log(fighter1.takeDamage());
-        console.log(
-          `${fighter1.name} healthPoints remaining ${fighter1.reduceHealthBy(
-            fighter2.strength
-          )}`
-        );
-      }
+
+// Creates a hero named thor, he gets a strength of 2
+const thor = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10000,
+  strength: 2,
+  name: 'Thor',
+  team: 'Forest Kingdom',
+  weapons: ['Bow', 'Dagger'],
+  language: 'Elvish'
+});
+
+// Creates a villain named thanos, gets a strength of 4 for the infinity stones
+const thanos = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10000,
+  strength: 4,
+  name: 'Thanos',
+  team: 'Forest Kingdom',
+  weapons: ['Bow', 'Dagger'],
+  language: 'Elvish'
+});
+
+// Gaming helper functions
+/**
+ * @param  {number} min - minimum
+ * @param  {number} max - maximum
+ * @return  {number} A random number between min and max
+ */
+function randomIndex(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Starts a fight between two Humanoids
+function combat(fighter1, fighter2) {
+  console.log(`Fight Started between ${fighter1.name} and ${fighter2.name}`);
+  let winner = '';
+  while (fighter1.healthPoints && fighter2.healthPoints) {
+    const index = randomIndex(1, 2);
+    if (index == 1) {
+      console.log(fighter2.takeDamage());
+      console.log(
+        `${fighter2.name} healthPoints remaining ${fighter2.reduceHealthBy(
+          fighter1.strength
+        )}`
+      );
+    } else {
+      console.log(fighter1.takeDamage());
+      console.log(
+        `${fighter1.name} healthPoints remaining ${fighter1.reduceHealthBy(
+          fighter2.strength
+        )}`
+      );
     }
-    winner = fighter1.healthPoints > 0 ? fighter1.name : fighter2.name;
-    console.log(`Fight Ended, ${winner} is the new champion!`);
   }
-  
-  // combat(thor, thanos);
-  
+  winner = fighter1.healthPoints > 0 ? fighter1.name : fighter2.name;
+  console.log(`Fight Ended, ${winner} is the new champion!`);
+}
+
+// combat(thor, thanos);
