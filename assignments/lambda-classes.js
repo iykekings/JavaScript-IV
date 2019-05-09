947933c2ba3839b9afcac07674c624a10fc95d53
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-09 22:54:09 +0100

    complete lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,52 +1,60 @@
 // CODE here for your Lambda Classes
-class Person{
-    constructor(personAttrs) {
-       this.name = personAttrs.name;
-       this.age = personAttrs.age;
-       this.location = personAttrs.location;
-       this.gender = personAttrs.gender;
-    }
-    speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
-    }
+class Person {
+  constructor(personAttrs) {
+    this.name = personAttrs.name;
+    this.age = personAttrs.age;
+    this.location = personAttrs.location;
+    this.gender = personAttrs.gender;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
 }
 
 class Instructor extends Person {
-    constructor(insAttrs) {
-        super(insAttrs);
-        this.specialty = insAttrs.specialty;
-        this.favLanguage = insAttrs.favLanguage;
-        this.catchPhrase = insAttrs.catchPhrase;
-    }
-    demo(subject) {
-        return `Today we are learning about ${subject}`
-    }
-    grade(student,subject) {
-        return `${student.name} receives a perfect score on ${subject}`
-    }
+  constructor(insAttrs) {
+    super(insAttrs);
+    this.specialty = insAttrs.specialty;
+    this.favLanguage = insAttrs.favLanguage;
+    this.catchPhrase = insAttrs.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
 }
 
 class Student extends Person {
-    constructor(stAttrs) {
-        this.previousBackground = stAttrs.previousBackground;
-        this.className = stAttrs.className;
-        this.favSubjects = stAttrs.favSubjects;
-    }
-    listsSubjects() {
-        return this.favSubjects;
-    }
-    PRAssignment(subject) {
-        return `${this.name} has submitted a PR for ${subject}`
-    }
-    sprintChallenge() {
-        return `${this.name} has begun sprint challenge on ${subject}`
-    }
+  constructor(stAttrs) {
+    this.previousBackground = stAttrs.previousBackground;
+    this.className = stAttrs.className;
+    this.favSubjects = stAttrs.favSubjects;
+  }
+  listsSubjects() {
+    this.favSubjects.forEach(singleSubject => console.log(singleSubject));
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+  sprintChallenge() {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
 }
 
 class ProjectManager extends Instructor {
-    constructor(pmAttrs) {
-        super(pmAttrs);
-        this.gradClassName = pmAttrs.gradClassName;
-        this.favInstructor = pmAttrs.favInstructor;
-    }
+  constructor(pmAttrs) {
+    super(pmAttrs);
+    this.gradClassName = pmAttrs.gradClassName;
+    this.favInstructor = pmAttrs.favInstructor;
+  }
+  standUp(channel) {
+    console.log(
+      `${this.name} announces to ${channel}, @channel standy times!​​​​​`
+    );
+  }
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  }
 }
